1ec03ff8605e727896879d513c7c0382fdb69232
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-12-13 23:28:16 -0500

    Add job, queue and pending spec.

--- lib/riaque.rb
+++ lib/riaque.rb
@@ -1,11 +1,13 @@
 require "riaque/version"
+require "riik"
 
 module Riaque
-  autoload :Job, "riaque/job"
+  autoload :Job,   "riaque/job"
+  autoload :Queue, "riaque/queue"
 
   class << self
     def enqueue(klass, *args)
-      true
+      Job.create(:class => klass, :payload => args).enqueue
     end
   end
 end
