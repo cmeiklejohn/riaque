9bc1118a30213caa4d73b5558d9f95e76679a93b
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-12-13 23:38:31 -0500

    Queue's have a name.

--- lib/riaque/queue.rb
+++ lib/riaque/queue.rb
@@ -1,5 +1,11 @@
 module Riaque
   class Queue
     include Riik::Document
+
+    property :name
+
+    def self.name_for(klass)
+      klass.instance_variable_get("@queue") || :default
+    end
   end
 end
--- spec/lib/riaque/queue_spec.rb
+++ spec/lib/riaque/queue_spec.rb
@@ -3,5 +3,15 @@ require 'spec_helper'
 module Riaque
   describe Queue do 
     subject { queue } 
+
+    let(:queue) { Queue } 
+
+    it 'knows the queue name to retrieve things for if set' do 
+      subject.name_for(VectorJob).should == :vectors
+    end
+
+    it 'knows the default queue name' do 
+      subject.name_for(StringJob).should == :default
+    end
   end
 end
