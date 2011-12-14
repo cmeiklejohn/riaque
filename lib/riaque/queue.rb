a0199862ff0ec6007cf915f7374b79bc912cd620
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-12-14 01:15:58 -0500

    Some more specs, documentation and queue instantiation.

--- lib/riaque/queue.rb
+++ lib/riaque/queue.rb
@@ -4,8 +4,31 @@ module Riaque
 
     property :name
 
+    # Returns the name of the queue to use for a particular job class.
+    # 
+    # @param [Class] job class
+    # @return [String]
+    #
     def self.name_for(klass)
       klass.instance_variable_get("@queue") || :default
+    end
+
+    # Returns the Queue for a particular job class.
+    # 
+    # @param [Class] job class
+    # @return [Queue]
+    #
+    def self.for(klass)
+      self.instance_for(name_for(klass))
+    end
+
+    # Return the instantiated object for a particular queue name.
+    #
+    # @param [String] queue name
+    # @return [Queue]
+    #
+    def self.instance_for(name)
+      self.new(:name => name)
     end
   end
 end
--- spec/lib/riaque/queue_spec.rb
+++ spec/lib/riaque/queue_spec.rb
@@ -13,5 +13,21 @@ module Riaque
     it 'knows the default queue name' do 
       subject.name_for(StringJob).should == :default
     end
+
+    it 'returns the queue for a particular job type' do 
+      subject.for(VectorJob).should be_an_instance_of(Queue)
+    end
+
+    pending 'has a name'
+
+    pending 'has a list of jobs'
+
+    context 'with an existing queue object in riak' do 
+
+      pending 'returns the queue for a particular job type'
+
+      pending 'adds a job to the queue'
+
+    end
   end
 end
