81daa5d383948fae6e837b2ef244119f3a1ac2e9
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-12-14 00:48:11 -0500

    Only new episoes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,7 @@ require 'vcr'
 VCR.config do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.stub_with :webmock
-  c.default_cassette_options = { :record => :all }
+  c.default_cassette_options = { :record => :new_episodes }
 end
 
 module Riaque
