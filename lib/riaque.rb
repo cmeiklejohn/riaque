eefdcd37d2718715e6bc2677d91534c6005daa60
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2011-12-19 23:43:59 -0500

    Add core extentions to add qualified_const_get.

--- lib/riaque.rb
+++ lib/riaque.rb
@@ -2,12 +2,21 @@ require "riaque/version"
 require "riik"
 
 module Riaque
-  autoload :Job,   "riaque/job"
-  autoload :Queue, "riaque/queue"
+  autoload :Job,     "riaque/job"
+  autoload :Queue,   "riaque/queue"
+  autoload :CoreExt, "riaque/core_ext"
 
   class << self
+
+    # Enqueue a job.
+    #
+    # @param  [Class]   job class.
+    # @param  [Array]   attributes.
+    # @return [Boolean]
+    #
     def enqueue(klass, *payload)
       Job.enqueue(klass, *payload)
     end
+
   end
 end
